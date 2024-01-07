8b585e04812b7e8e6c3a502fbb6e529df0eb17de
madhura-punde <madhupunde95@gmail.com>
2024-01-07 12:36:22 +0530

    vite.config.js updated

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+<<<<<<< Updated upstream
   plugins: [react()],
   optimizeDeps: {
     include: ['@mui/material/Tooltip'],
   }
 })
 
+=======
+  // plugins: [react()],
+  ptimizeDeps: {
+    include: ["@emotion/react", "@emotion/styled", "@mui/material/Tooltip"],
+  },
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+      babel: {
+        plugins: ["@emotion/babel-plugin"],
+      },
+    }),
+  ],
+});
+>>>>>>> Stashed changes
