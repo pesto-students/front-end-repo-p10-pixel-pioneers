56ee695e88665f1c94ad475b4beb8a700beb4284
madhura-punde <madhupunde95@gmail.com>
2024-01-31 10:07:51 +0530

    Added Testimonials Data-actual reviews

--- src/pages/Home/Components/TestimonialsData.js
+++ src/pages/Home/Components/TestimonialsData.js
@@ -1,23 +1,26 @@
 export const testimonials = [
-    {
-      name: 'John Doe',
-      profile: 'Web Developer',
-      image: 'https://via.placeholder.com/150', // Replace with actual user image URL
-      rating: 4,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim ad minim veniam.',
-    },
-    {
-      name: 'Alice Smith',
-      profile: 'Designer',
-      image: 'https://via.placeholder.com/150', // Replace with actual user image URL
-      rating: 5,
-      description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    },
-    {
-      name: 'Emily Johnson',
-      profile: 'Marketing Manager',
-      image: 'https://via.placeholder.com/150', // Replace with actual user image URL
-      rating: 4.5,
-      description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-  ];
+  {
+    name: "John Doe",
+    profile: "Web Developer",
+    image: "https://via.placeholder.com/150", // Replace with actual user image URL
+    rating: 4,
+    description:
+      "love the friendly staff and how clear it is that they care about the space and make it a clean and welcoming environment for the individuals that rent the space out.",
+  },
+  {
+    name: "Alice Smith",
+    profile: "Designer",
+    image: "https://via.placeholder.com/150", // Replace with actual user image URL
+    rating: 5,
+    description:
+      "Wide availability of spaces at all hours to book and consistent user experience across all locations.",
+  },
+  {
+    name: "Emily Johnson",
+    profile: "Marketing Manager",
+    image: "https://via.placeholder.com/150", // Replace with actual user image URL
+    rating: 4.5,
+    description:
+      "The design of the locations, IT facilities, flexibility and availability of the locations around the world. In some cases and some locations are great on the events and promotions they bring to their users.",
+  },
+];
