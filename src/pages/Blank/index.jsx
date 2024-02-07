1a4f34c78d6f1ef98f0a81af038189f21a8d8d7e
madhura-punde <madhupunde95@gmail.com>
2024-02-07 22:36:42 +0530

    carousal-2 types

--- src/pages/Blank/index.jsx
+++ src/pages/Blank/index.jsx
@@ -1,45 +1,123 @@
-import React, { useRef, useState } from 'react';
-import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
+// import Slider from "react-slick";
+import React from "react";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+import Carousel from "react-carousel-mui";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import { makeStyles } from "@mui/styles";
+import Slider from "./Slider";
 
+const CustomCard = ({ url }) => {
+  return (
+    <Card>
+      <CardMedia component="img" image={url} />
+    </Card>
+  );
+};
 
-import "./styles.css"
+const useStyles = makeStyles((theme) => ({
+  sliderContainer: {
+    // marginTop: "20px",
+    marginBottom: "15px",
+  },
+  imageSlider: {
+    // width: "100%",
+    // maxWidth: "200px",
+    maxHeight: "450px",
+    height: "450px",
+    cursor: "pointer",
+  },
+}));
 
-const cardDetails = [
-    {
-        name: "Kartik Dhunisinghani",
-        description: `This impressive paella is a perfect party dish and a fun meal
-        to cook together with your guests. Add 1 cup of frozen..`
-    },
-    {
-        name: "Yash Dhunisinghani",
-        description: `This impressive paella is a perfect party dish and a fun meal
-        to cook together with your guests. Add 1 cup of frozen..`
-    },
-    {
-        name: "Harish Dhunisinghani",
-        description: `This impressive paella is a perfect party dish and a fun meal
-        to cook together with your guests. Add 1 cup of frozen..`
-    },
-    {
-        name: "Nikita Dhunisinghani",
-        description: `This impressive paella is a perfect party dish and a fun meal
-        to cook together with your guests. Add 1 cup of frozen..`
-    },
-]
-
+const images = [
+  {
+    photo:
+      "https://cdn.pixabay.com/photo/2022/01/25/04/42/bird-6965228_1280.jpg ",
+    label: "image1",
+  },
+  {
+    photo:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fnature%2F&psig=AOvVaw0vOS7Ld7G28NWmUKf-mQUn&ust=1707326102702000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKiBp8Cbl4QDFQAAAAAdAAAAABAE",
+    label: "image2",
+  },
+  {
+    photo:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fnature%2F&psig=AOvVaw0vOS7Ld7G28NWmUKf-mQUn&ust=1707326102702000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKiBp8Cbl4QDFQAAAAAdAAAAABAE",
+    label: "image3",
+  },
+  {
+    photo:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fnature%2F&psig=AOvVaw0vOS7Ld7G28NWmUKf-mQUn&ust=1707326102702000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKiBp8Cbl4QDFQAAAAAdAAAAABAE",
+    label: "image4",
+  },
+  {
+    photo:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fnature%2F&psig=AOvVaw0vOS7Ld7G28NWmUKf-mQUn&ust=1707326102702000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKiBp8Cbl4QDFQAAAAAdAAAAABAE",
+    label: "image2",
+  },
+  {
+    photo:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fnature%2F&psig=AOvVaw0vOS7Ld7G28NWmUKf-mQUn&ust=1707326102702000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKiBp8Cbl4QDFQAAAAAdAAAAABAE",
+    label: "image3",
+  },
+];
 
 const Blank = () => {
+  const classes = useStyles();
+  const srcList = "https://cdn.pixabay.com/photo/2022/01/25/04/42/bird-6965228_1280.jpg "
+    .repeat(10)
+    .split(" ")
+    .slice(0, 10);
 
-    return (
-        <>
-            <Stack>
-                
-            </Stack>
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slickNext: true,
+    slickPrevious: true,
+    swipe: true,
+    verticalSwiping: false,
+  };
 
-        </>
-    );
-}   
-
+  return (
+    <>
+      <Carousel
+        items={srcList}
+        className={classes.carouselContainer}
+        itemsPerPage={{
+          xs: 2,
+          sm: 2,
+          tablet: 2,
+          md: 3,
+          lg: 3,
+          xl: 3,
+        }}
+        itemRenderer={(item) => <CustomCard url={item} />}
+      />
+      <br />
+      <Slider
+        className={classes.imageSlider}
+        sliderItems={images}
+        sliderType="fade"
+        sliderAutoPlay={true}
+        sliderStopOnHover={false}
+        slideNavigatorsHide={true}
+        sliderIndicators
+        sliderIndicatorsStyle={{
+          padding: "10px",
+        }}
+        // sliderActiveIndicator={{
+        //   color: "pink",
+        // }}
+      />
+    </>
+  );
+};
 
 export default Blank;
