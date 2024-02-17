bffdb79740d400cd565597223718e7c42759cae2
madhura-punde <madhupunde95@gmail.com>
2024-02-17 20:03:13 +0530

    Fixed margin for Filter in PropertyList component

--- src/pages/PropertyList/index.jsx
+++ src/pages/PropertyList/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Stack from "@mui/material/Stack";
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
 import PropertyCard from "../../components/PropertyCard";
 import Filters from "../../components/Filters";
@@ -9,78 +9,94 @@ import Filters from "../../components/Filters";
 import { propertyList } from "../../api/property";
 
 const PropertyList = () => {
+  const [loading, setLoading] = useState(false);
+  const [filters, setFilters] = useState({
+    city: "All",
+    sort: "Low to High",
+  });
+  const [properties, setProperties] = useState([]);
+  const [error, setError] = useState("");
 
-    const [loading, setLoading] = useState(false);
-    const [filters, setFilters] = useState({
-        city: "All",
-        sort: "Low to High"
-    });
-    const [properties, setProperties] = useState([]);
-    const [error, setError] = useState("");
+  const fetchData = async (filter) => {
+    const response = await propertyList(filter);
 
-    const fetchData = async (filter) => {
-
-        const response = await propertyList(filter);
-
-        if (response.success) {
-            setProperties(response.data);
-        } else {
-            setError(response.message);
-        }
-
-    };
-
-    useEffect(() => {
-
-        setLoading(prev => true);
-        setProperties([]);
-        (async function () {
-            await fetchData(filters);
-        })();
-        setLoading(prev => false);
-
-    }, [filters]);
-
-    if (loading) {
-        return (
-            <>
-                <Box sx={{ display: 'flex', justifyContent: "center", alignContent: "center" }} margin={10}>
-                    <CircularProgress />
-                </Box>
-            </>
-        )
+    if (response.success) {
+      setProperties(response.data);
+    } else {
+      setError(response.message);
     }
+  };
 
-    if (properties.length === 0) {
-        return (
-            <>
-                {/* <h1 style={{ textAlign: "center" }}>Property List</h1> */}
-                <Filters handleFilter={setFilters} />
-                <Box sx={{ display: 'flex', justifyContent: "center", alignContent: "center" }} margin={10}>
-                    <CircularProgress />
-                </Box>
-            </>
-        )
-    }
+  useEffect(() => {
+    setLoading((prev) => true);
+    setProperties([]);
+    (async function() {
+      await fetchData(filters);
+    })();
+    setLoading((prev) => false);
+  }, [filters]);
 
+  if (loading) {
     return (
+      <>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignContent: "center",
+          }}
+          margin={10}
+        >
+          <CircularProgress />
+        </Box>
+      </>
+    );
+  }
 
-        <>
-            <h1 style={{ textAlign: "center" }}>Property List</h1>
-            <Filters handleFilter={setFilters} />
+  if (properties.length === 0) {
+    return (
+      <>
+        {/* <h1 style={{ textAlign: "center" }}>Property List</h1> */}
+        <Filters handleFilter={setFilters} />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignContent: "center",
+          }}
+          margin={10}
+        >
+          <CircularProgress />
+        </Box>
+      </>
+    );
+  }
 
-            {/* {PropertyCards} */}
-            <Stack margin={3} direction={{ xs: "column", md: "row" }} gap={1} justifyContent={"center"} alignContent={"center"} flexWrap={"wrap"}>
-                {
-                    properties.map((property, propertyIndex) => {
-                        return (
-                            <PropertyCard key={`property-card-${propertyIndex}`} propertyDetails={property} />
-                        )
-                    })
-                }
-            </Stack>
-        </>
-    )
-}
+  return (
+    <div style={{ marginTop: "90px" }}>
+      {/* <h1 style={{ textAlign: "center" }}>Property List</h1> */}
+      <Filters handleFilter={setFilters} />
+
+      {/* {PropertyCards} */}
+      <Stack
+        margin={3}
+        direction={{ xs: "column", md: "row" }}
+        gap={1}
+        justifyContent={"center"}
+        alignContent={"center"}
+        flexWrap={"wrap"}
+      >
+        {properties.map((property, propertyIndex) => {
+          return (
+            <PropertyCard
+              key={`property-card-${propertyIndex}`}
+              propertyDetails={property}
+            />
+          );
+        })}
+      </Stack>
+    </div>
+  );
+};
 
 export default PropertyList;
